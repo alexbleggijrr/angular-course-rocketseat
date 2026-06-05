51a821e3e301c1e596ba2cf7f073394e8da84419
alexbleggijrr <alexbleggijrr@gmail.com>
2026-06-05 16:09:44 -0300

    using event binding

--- 01-first-steps/02-angular-fundamentals/my-new-project/src/app/app.component.ts
+++ 01-first-steps/02-angular-fundamentals/my-new-project/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonFlatComponent } from './button-flat/button-flat.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
-import { MyButtonComponent } from './my-button/my-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MyButtonComponent, ButtonFlatComponent, EventBindingComponent],
+  imports: [EventBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
