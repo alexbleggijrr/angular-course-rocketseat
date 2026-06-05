7d56e9b5dae22b76f5a0afc029e925f7bbe4a0bc
alexbleggijrr <alexbleggijrr@gmail.com>
2026-06-05 11:16:28 -0300

    component file structure

---
+++ 01-first-steps/02-angular-fundamentals/my-new-project/src/app/my-button/my-button.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-button',
+  imports: [],
+  templateUrl: './my-button.component.html',
+  styleUrl: './my-button.component.scss',
+})
+export class MyButtonComponent {
+  limpar() {
+    console.log('Método limpar');
+  }
+
+  filtrar() {
+    console.log('Método limpar');
+  }
+}
